96b50393eef3a54ae0876508d1a3b9f36985c116
Hadia Khokhar <workwithhadiaa@outlook.com>
2024-08-05 10:50:25 +0500

    Added the Hero Section : Text Section

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    const pageLinks = document.getElementById('page-links');
+    var hamburgerMenu = document.getElementById('hamburger-menu');
+    var pageLinks = document.getElementById('page-links');
   
     hamburgerMenu.addEventListener('click', () => {
       pageLinks.classList.toggle('active');
