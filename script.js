78d9283a780f55aade6ca51b64dcb2222290a0b4
Hadia Khokhar <workwithhadiaa@outlook.com>
2024-08-09 14:10:51 +0500

    Added About me Section (Completed)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// For Menu Button
 document.addEventListener('DOMContentLoaded', () => {
     var hamburgerMenu = document.getElementById('hamburger-menu');
     var pageLinks = document.getElementById('page-links');
@@ -7,4 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
       hamburgerMenu.classList.toggle('active');
     });
   });
-  
+// For Read More and Read Less  
+  function toggleText() {
+    const text = document.getElementById('text');
+    const readMore = document.getElementById('read-more');
+
+    if (readMore.textContent === "Read More") {
+        text.style.display = 'block';
+        text.style.webkitLineClamp = 'unset';
+        readMore.textContent = "Show Less";
+    } else {
+        text.style.display = '-webkit-box';
+        text.style.webkitLineClamp = '5';
+        readMore.textContent = "Read More";
+    }
+}
