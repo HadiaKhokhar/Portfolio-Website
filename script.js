389a6a2e869a9714af67802dc2e2e12ecf28cca6
Hadia Khokhar <workwithhadiaa@outlook.com>
2024-08-11 18:49:43 +0500

    Added Work Section

--- script.js
+++ script.js
@@ -1,12 +1,66 @@
-// For Menu Button
+
 document.addEventListener('DOMContentLoaded', () => {
+  // For Menu Button
     var hamburgerMenu = document.getElementById('hamburger-menu');
     var pageLinks = document.getElementById('page-links');
+
   
     hamburgerMenu.addEventListener('click', () => {
       pageLinks.classList.toggle('active');
       hamburgerMenu.classList.toggle('active');
     });
+  // For hide and show card
+  var allBtn = document.getElementById('all-button');
+  var uiBtn = document.getElementById('ui-button');
+  var uxBtn = document.getElementById('ux-button');
+  var webDesignBtn = document.getElementById('web-design-button');
+  var uxCard = document.getElementById('ux-card');
+  var uiCard = document.getElementById('ui-card');
+  var webDesigncard = document.getElementById('web-design-card');
+  var buttonlist = [allBtn, uiBtn, uxBtn, webDesignBtn];
+
+  // Function to reset all button backgrounds
+  function resetButtonBackgrounds() {
+      buttonlist.forEach(function(button) {
+          button.style.backgroundColor = 'rgb(57, 62, 70)';  // Reset to default color
+      });  // Corrected placement of the closing brace
+  }
+   // For active button
+  function activBtnbackground(activeBtn){
+    activeBtn.style.backgroundColor = 'rgb(0, 173, 181)'
+  } 
+
+
+  allBtn.addEventListener('click', function() {
+    resetButtonBackgrounds()
+    uiCard.style.display= '';
+    uxCard.style.display = '';
+    webDesigncard.style.display = '';
+    activBtnbackground(allBtn)
+    });
+  uiBtn.addEventListener('click', function() {
+    resetButtonBackgrounds()
+    uxCard.style.display = 'none';
+    webDesigncard.style.display = 'none';
+    uiCard.style.display= '';
+    activBtnbackground(uiBtn)
+    });
+  uxBtn.addEventListener('click', function() {
+    resetButtonBackgrounds()
+    uiCard.style.display= 'none';
+    webDesigncard.style.display = 'none';
+    uxCard.style.display = '';
+    activBtnbackground(uxBtn)
+    });
+  webDesignBtn.addEventListener('click', function() {
+    resetButtonBackgrounds()
+    uiCard.style.display= 'none';
+    uxCard.style.display = 'none';
+    webDesigncard.style.display = '';
+    activBtnbackground(webDesignBtn)
+    });
+
+
   });
 // For Read More and Read Less  
   function toggleText() {
